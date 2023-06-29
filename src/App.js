b98d527d9bddba01733a2a7988829faa6b37d23e
ColetonS <coleton.m.smith@gmail.com>
2023-06-29 15:07:24 -0600

    started on forms

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/newUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 
 function App() {
   return (
@@ -13,16 +17,17 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
-
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
-
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
+          </Route>
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
         {/* End Dash */}
---
+++ src/features/notes/EditNoteForm.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+const EditNoteForm = ({ note, users }) => {
+  const [updateNote, { isLoading, isSuccess, isError, error }] =
+    useUpdateNoteMutation();
+
+  const [
+    deleteNote,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
+  const [completed, setCompleted] = useState(note.completed);
+  const [userId, setUserId] = useState(note.user);
+
+  useEffect(() => {
+    if (isSuccess || isDelSuccess) {
+      setTitle("");
+      setText("");
+      setUserId("");
+      navigate("/dash/notes");
+    }
+  }, [isSuccess, isDelSuccess, navigate]);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onTextChange = (e) => setText(e.target.value);
+  const onCompletedChanged = (e) => setCompleted((prev) => !prev);
+  const onUserIdChanged = (e) => setUserId(e.target.value);
+
+  const canSave = [title, text, userId].every(Boolean) && !isLoading;
+
+  const onSaveNoteClicked = async (e) => {
+    if (canSave) {
+      await updateNote({ id: note.id, user: userId, title, text, completed });
+    }
+  };
+
+  const onDeleteNoteClicked = async () => {
+    await deleteNote({ id: note.id });
+  };
+
+  const created = new Date(note.createdAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  const updated = new Date(note.updatedAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  const options = users.map((user) => {
+    return (
+      <option key={user.id} value={user.id}>
+        {user.username}
+      </option>
+    );
+  });
+};
